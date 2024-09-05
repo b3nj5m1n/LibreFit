00628e66a8bbcfea7bd2a93a2192922ca1a4e208
IamDg <180081466+IamDg@users.noreply.github.com>
2024-09-05 11:22:03 +0200

    SDK upgraded to 35; Dependencies updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "org.librefit"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "org.librefit"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
