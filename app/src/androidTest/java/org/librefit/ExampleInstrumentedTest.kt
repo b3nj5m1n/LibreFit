769809ef7a63cd57998086fb32d7f7afe678372b
IamDg <180081466+IamDg@users.noreply.github.com>
2025-07-28 17:49:54 +0200

    test: fix package name in instrumented test

--- app/src/androidTest/java/org/librefit/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/org/librefit/ExampleInstrumentedTest.kt
@@ -21,7 +21,7 @@ package org.librefit
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -32,11 +32,10 @@ import org.junit.runner.RunWith
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    //TODO: add testing
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("org.librefit", appContext.packageName)
+        assertEquals("org.librefit.app", appContext.packageName)
     }
 }
