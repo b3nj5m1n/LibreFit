be6a7e188cbcace5e18d7d682a93fe084b3f561d
IamDg <180081466+IamDg@users.noreply.github.com>
2025-07-30 22:56:56 +0200

    feat: filter card clickable

--- app/src/main/java/org/librefit/ui/screens/exercises/FiltersCard.kt
+++ app/src/main/java/org/librefit/ui/screens/exercises/FiltersCard.kt
@@ -30,6 +30,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
@@ -43,13 +44,12 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
@@ -80,9 +80,15 @@ fun FiltersCard(
     updateFilter: (FilterValue) -> Unit,
     filterValue: FilterValue
 ) {
-    var iconRotation by rememberSaveable { mutableFloatStateOf(0f) }
+    val iconRotation = if (isFilterExpanded) 0f else 180f
 
-    OutlinedCard {
+    OutlinedCard(
+        modifier = Modifier
+            .clip(CardDefaults.outlinedShape)
+            .clickable {
+                updateCardExpansion()
+            }
+    ) {
         Row(
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically,
@@ -106,7 +112,6 @@ fun FiltersCard(
             IconButton(
                 onClick = {
                     updateCardExpansion()
-                    iconRotation = if (isFilterExpanded) 0f else 180f
                 }
             ) {
                 Icon(
