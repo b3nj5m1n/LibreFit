0e7f3ddb7ba1593c1fd74bb90fd4a4fbb3fb00bd
IamDg <iamdg.dev@proton.me>
2026-04-26 16:33:16 +0200

    feat: add undo and clear functionality to InputModalBottomSheet

--- app/src/main/java/org/librefit/ui/components/modalBottomSheets/InputModalBottomSheet.kt
+++ app/src/main/java/org/librefit/ui/components/modalBottomSheets/InputModalBottomSheet.kt
@@ -8,6 +8,7 @@
 
 package org.librefit.ui.components.modalBottomSheets
 
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -16,6 +17,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ButtonGroup
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.MaterialTheme
@@ -30,11 +32,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.librefit.R
 import org.librefit.enums.userPreferences.ThemeMode
+import org.librefit.ui.components.LibreFitButton
 import org.librefit.ui.components.NumberPicker
 import org.librefit.ui.models.InputModalBottomSheetState
 import org.librefit.ui.theme.LibreFitTheme
@@ -46,6 +50,8 @@ fun InputModalBottomSheet(
     state: InputModalBottomSheetState,
     onValueChange: (InputModalBottomSheetState) -> Unit
 ) {
+    // Save initial state so user can restore it
+    val initialState = remember { state }
 
     ModalBottomSheet(
         onDismissRequest = onDismiss,
@@ -218,6 +224,69 @@ fun InputModalBottomSheet(
                                 )
                             }
                         }
+                    }
+                }
+            }
+
+            item {
+                val interactionSources = remember { List(2) { MutableInteractionSource() } }
+                Row {
+                    ButtonGroup(
+                        overflowIndicator = {}
+                    ) {
+                        customItem(
+                            menuContent = {},
+                            buttonGroupContent = {
+                                LibreFitButton(
+                                    modifier = Modifier
+                                        .weight(1f)
+                                        .animateWidth(interactionSources[0]),
+                                    text = stringResource(R.string.undo),
+                                    icon = painterResource(R.drawable.ic_undo),
+                                    interactionSource = interactionSources[0]
+                                ) {
+                                    onValueChange(initialState)
+                                }
+                            }
+                        )
+                        customItem(
+                            menuContent = {},
+                            buttonGroupContent = {
+                                LibreFitButton(
+                                    modifier = Modifier
+                                        .weight(1f)
+                                        .animateWidth(interactionSources[1]),
+                                    text = stringResource(R.string.clear),
+                                    icon = painterResource(R.drawable.ic_cancel),
+                                    interactionSource = interactionSources[1],
+                                    elevated = false
+                                ) {
+                                    onValueChange(
+                                        when (state) {
+                                            is InputModalBottomSheetState.HoursMinutesSeconds -> state.copy(
+                                                hours = state.hoursRange.first(),
+                                                minutes = state.minutesRange.first(),
+                                                seconds = state.secondsRange.first(),
+                                            )
+
+                                            is InputModalBottomSheetState.MinutesSeconds -> state.copy(
+                                                minutes = state.minutesRange.first(),
+                                                seconds = state.secondsRange.first(),
+                                            )
+
+                                            is InputModalBottomSheetState.Reps -> state.copy(
+                                                reps = state.repsRange.first()
+                                            )
+
+                                            is InputModalBottomSheetState.Weight -> state.copy(
+                                                integerWeight = state.integerWeightRange.first(),
+                                                decimalWeight = state.decimalWeightRange.first()
+                                            )
+                                        }
+                                    )
+                                }
+                            }
+                        )
                     }
                 }
             }
