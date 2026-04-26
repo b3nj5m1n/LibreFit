8e48e99a5c2d093de682b2f2eb691fb8642626c0
IamDg <iamdg.dev@proton.me>
2026-04-26 16:34:12 +0200

    feat: add validation to `InputModalBottomSheetState` and update weight range

--- app/src/main/java/org/librefit/ui/models/InputModalBottomSheetState.kt
+++ app/src/main/java/org/librefit/ui/models/InputModalBottomSheetState.kt
@@ -11,6 +11,7 @@ package org.librefit.ui.models
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.toImmutableList
 import java.lang.Math.toIntExact
+import kotlin.math.pow
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.hours
 import kotlin.time.Duration.Companion.minutes
@@ -23,6 +24,16 @@ sealed class InputModalBottomSheetState {
         val minutesRange: ImmutableList<Int> = (0..59).toImmutableList(),
         val secondsRange: ImmutableList<Int> = (0..59).toImmutableList()
     ) : InputModalBottomSheetState() {
+        init {
+            require(minutes in minutesRange) {
+                "minutes $minutes must be in minutesRange: $minutesRange"
+            }
+            require(seconds in secondsRange) {
+                "seconds $seconds must be in secondsRange: $secondsRange"
+            }
+        }
+
+
         val duration: Duration get() = minutes.minutes + seconds.seconds
 
         val totalSeconds: Int get() = toIntExact(duration.inWholeSeconds)
@@ -31,17 +42,35 @@ sealed class InputModalBottomSheetState {
     data class Weight(
         val integerWeight: Int = 0,
         val decimalWeight: Int = 0,
-        val integerWeightRange: ImmutableList<Int> = (0..300).toImmutableList(),
+        val integerWeightRange: ImmutableList<Int> = (0..999).toImmutableList(),
         val decimalWeightRange: ImmutableList<Int> = (0..99).toImmutableList()
     ) : InputModalBottomSheetState() {
 
-        val totalWeight: Double get() = integerWeight + (decimalWeight / 100.0)
+        init {
+            require(integerWeight in integerWeightRange) {
+                "integerWeight $integerWeight must be in integerWeightRange: $integerWeightRange"
+            }
+            require(decimalWeight in decimalWeightRange) {
+                "decimalWeight $decimalWeight must be in decimalWeightRange: $decimalWeightRange"
+            }
+        }
+
+        private val divisor: Double =
+            10.0.pow(decimalWeightRange.max().toString().length.toDouble())
+
+        val totalWeight: Double get() = integerWeight + (decimalWeight / divisor)
     }
 
     data class Reps(
         val reps: Int = 0,
         val repsRange: ImmutableList<Int> = (0..999).toImmutableList()
-    ) : InputModalBottomSheetState()
+    ) : InputModalBottomSheetState() {
+        init {
+            require(reps in repsRange) {
+                "reps $reps must be in repsRange: $repsRange"
+            }
+        }
+    }
 
     data class HoursMinutesSeconds(
         val hours: Int = 0,
@@ -51,6 +80,18 @@ sealed class InputModalBottomSheetState {
         val minutesRange: ImmutableList<Int> = (0..59).toImmutableList(),
         val secondsRange: ImmutableList<Int> = (0..59).toImmutableList()
     ) : InputModalBottomSheetState() {
+        init {
+            require(hours in hoursRange) {
+                "hours $hours must be in hoursRange: $hoursRange"
+            }
+            require(minutes in minutesRange) {
+                "minutes $minutes must be in minutesRange: $minutesRange"
+            }
+            require(seconds in secondsRange) {
+                "seconds $seconds must be in secondsRange: $secondsRange"
+            }
+        }
+
         val duration: Duration get() = hours.hours + minutes.minutes + seconds.seconds
 
         val totalSeconds: Int get() = toIntExact(duration.inWholeSeconds)
